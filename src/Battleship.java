776aac2cc8e745a48300d9280bb84df9fb9b2966
exteriorduke319 <yarnevictanner@gmail.com>
2024-01-31 12:20:56 -0500

    initial

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,2 +1,90 @@
-package PACKAGE_NAME;public class Battleship {
+import java.util.Scanner;
+
+public class Battleship {
+   public static boolean startGame = false;
+    public static void main(String[] args) {
+        Board gameBoard = new Board();
+
+        Scanner drake = new Scanner(System.in);
+        String playerInput;
+        int rowInput;
+        int colInput;
+        int lenInput;
+        boolean horizontalCheck;
+        boolean startMenu = true;
+
+
+
+
+        while (startMenu) {
+            playerInput = InputHelper.getNonZeroLenString(drake, "Player: Enter \"a\" to add a ship, \"b\" to see the board, \"p\" to play, or \"q\" to quit.");
+            if(playerInput.equalsIgnoreCase("a")) {
+                rowInput = InputHelper.getInt(drake, "What row would you like this ship to be in? [1-10]") - 1;
+                colInput = InputHelper.getInt(drake, "What column would you like this ship to be in? [1-10]") - 1;
+                lenInput = InputHelper.getInt(drake, "How long would you like this ship to be? [1-10]");
+                horizontalCheck = InputHelper.getYNConfirm(drake, "Would you like the ship to be horizontal?  Y or N.");
+                if (gameBoard.addShip(rowInput, colInput, lenInput, horizontalCheck)) {
+                    System.out.println("New ship added!");
+                } else {
+                    System.out.println("Can't put a ship there!");
+                }
+            }
+
+            if(playerInput.equalsIgnoreCase("b")) {
+                System.out.println(gameBoard.toString());
+            }
+
+            if(playerInput.equalsIgnoreCase("p")) {
+                if (gameBoard.foundShip(3) && gameBoard.foundShip(4)){
+                    System.out.println("OK, let's play!");
+                    startMenu = false;
+                    startGame = true;
+                } else {
+                    System.out.println("You need a ship of length 3 and 4 before you can play!");
+                }
+            }
+
+            if(playerInput.equalsIgnoreCase("q")) {
+                startMenu = false;
+                System.out.println("Thanks for playing!");
+            }
+        }
+
+        while (startGame) {
+            playerInput = InputHelper.getNonZeroLenString(drake, "Player: Press \"s\" to shoot at a square, \"b\" to see the board, or \"q\" to quit.");
+
+            if(playerInput.equalsIgnoreCase("s")) {
+                rowInput = InputHelper.getInt(drake, "What row would you like to shoot? [1-10]") - 1;
+                colInput = InputHelper.getInt(drake, "What column would you like to shoot? [1-10]") - 1;
+                int result = gameBoard.shoot(rowInput, colInput);
+
+                if (result == 0) {
+                    System.out.println("Miss!");
+                } else if (result == 1) {
+                    System.out.println("Hit!");
+                } else if (result == 2) {
+                    System.out.println("Already guessed that spot!");
+                } else if (result == (-1)) {
+                    System.out.println("Invalid coordinates!");
+                }
+            }
+
+            if(playerInput.equalsIgnoreCase("b")) {
+                System.out.println(gameBoard.toString());
+            }
+
+            if(playerInput.equalsIgnoreCase("q")) {
+                startGame = false;
+                System.out.println("Thanks for playing!");
+
+            }
+
+            if (gameBoard.gameOver()) {
+                startGame = false;
+                System.out.println(gameBoard.toString());
+                System.out.println("Game Over!  You hit all of the ships!");
+            }
+        }
+
+    }
 }
--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,127 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Board {
+    private String[][] squares = new String[10][10];
+
+    public String toString() {
+        StringBuilder boardOut = new StringBuilder(); // I had to use ChatGPT to help with this including the first for loop.  Couldn't figure out how to do it and you weren't here
+
+        if (!Battleship.startGame) {
+            for (int i = 0; i < squares.length; i++) {
+                String[] row = squares[i];
+                for (int j = 0; j < row.length; j++) {
+                    String value = row[j];
+                    boardOut.append(value).append(" ");
+                }
+                boardOut.append("\n");
+            }
+        } else {
+            for (int i = 0; i < squares.length; i++) {
+                String[] row = squares[i];
+                for (int j = 0; j < row.length; j++) {
+                    String col = row[j];
+                    if (squares[i][j].equals("b")) {
+                        boardOut.append("- ");
+                    } else {
+                        boardOut.append(col).append(" ");
+                    }
+                }
+                boardOut.append("\n");
+            }
+        }
+
+        return boardOut.toString();
+    }
+    public boolean addShip(int row, int col, int len, boolean horizontal) {
+        if (horizontal) {
+            for(int i = 0; i < len; i++) {
+                if (squares[row][col + i].equals("b") || col+i > squares.length) {
+                    return false;
+                }
+
+                squares[row][col+i] = "b";
+            }
+
+        } else {
+            for(int i = 0; i < len; i++) {
+                if (squares[row + i][col].equals("b") || row+i > squares.length) {
+                    return false;
+                }
+
+                squares[row+i][col] = "b";
+            }
+        }
+
+
+
+        return true;
+
+    }
+
+    public boolean foundShip(int len) {
+        int counter = 0;
+
+        for(int r = 0; r < squares.length; r++){
+            for(int c = 0; c < squares.length; c++){
+                if (squares[r][c].contains("b")){
+                    counter++;
+                } else {
+                    counter = 0;
+                }
+
+                if (counter == len) {
+                    return true;
+                }
+
+            }
+        }
+
+        for(int c = 0; c < squares.length; c++){
+            for(int r = 0; r < squares.length; r++){
+                if (squares[r][c].contains("b")){
+                    counter++;
+                } else {
+                    counter = 0;
+                }
+
+                if (counter == len) {
+                    return true;
+                }
+
+            }
+        }
+
+        return false;
+    }
+
+    public int shoot(int row, int col) {
+        if (squares[row][col].equals("b")) {
+            squares[row][col] = "x";
+            return 1;
+        } else if (squares[row][col].equals("-")){
+            squares[row][col] = "m";
+            return 0;
+        } else if (squares[row][col].equals("x") || squares[row][col].equals("m")) {
+            return 2;
+        } else {
+            return (-1);
+        }
+    }
+
+    public boolean gameOver() {
+        for(int r = 0; r < squares.length; r++){
+            for(int c = 0; c < squares.length; c++){
+                if(squares[r][c].equals("b")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public Board() {
+        for (int r = 0; r < squares.length; r++) {
+            for (int c = 0; c < squares[0].length; c++) {
+                squares[r][c] = "-";
+            }
+        }
     }
 }
